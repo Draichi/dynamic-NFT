ab0b8f80bfaf90b32aff5361239cc84fefd52709
draichi <lucasdraichi@gmail.com>
2021-03-27 14:40:02 -0300

    feat: adds character

--- contracts/DungeonsAndDragonsCharacter.sol
+++ contracts/DungeonsAndDragonsCharacter.sol
@@ -12,6 +12,26 @@ contract DungeonsAndDragonsCharacter is ERC721, VRFConsumerBase {
 
     uint256 public randomResult;
 
+    struct Character {
+        uint256 strength;
+        uint256 speed;
+        uint256 stamina;
+        string name;
+    }
+
+    Character[] public characters;
+
+    // mappings
+
+
+    /**
+     * Constructor inherits VRFConsumerBase
+     *
+     * Network: Rinkeby
+     * Chainlink VRF Coordinator address: 0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B
+     * LINK token address:                0x01BE23585060835E02B77ef475b0Cc51aA1e0709
+     * Key Hash: 0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311
+     */
     constructor(
         address _VRFCoordinator,
         address _linkTokenAddress,
